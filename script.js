d15671823c7750cfa699146ad546d789f03243d0
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-05-29 16:00:37 +0200

    millestone 0

---
+++ script.js
@@ -0,0 +1,39 @@
+/*
+Wayne Barnett   Founder & CEO      wayne-barnett-founder-ceo.jpg
+Angela Caroll     Chief Editor      angela-caroll-chief-editor.jpg
+Walter Gordon     Office Manager    walter-gordon-office-manager.jpg
+Angela Lopez      Social Media      Manager  angela-lopez-social-media-manager.jpg
+Scott Estrada     Developer         scott-estrada-developer.jpg
+Barbara Ramos     Graphic Designer  barbara-ramos-graphic-designer.jpg
+*/
+
+const team = [
+
+    {
+        name : 'Wayne Barnett',
+        rule : 'Founder & CEO ',
+        image : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name : 'Wayne Barnett',
+        rule : 'Founder & CEO ',
+        image : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name : 'Wayne Barnett',
+        rule : 'Founder & CEO ',
+        image : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name : 'Wayne Barnett',
+        rule : 'Founder & CEO ',
+        image : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name : 'Wayne Barnett',
+        rule : 'Founder & CEO ',
+        image : 'wayne-barnett-founder-ceo.jpg'
+    },
+];
+
+console.log(team);
