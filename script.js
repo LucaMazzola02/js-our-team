455e03b64ada6fa866a944c1dccbd673b688516f
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-05-29 16:18:58 +0200

    millestone 1

--- script.js
+++ script.js
@@ -12,28 +12,44 @@ const team = [
     {
         name : 'Wayne Barnett',
         rule : 'Founder & CEO ',
-        image : 'wayne-barnett-founder-ceo.jpg'
+        image : 'wayne-barnett-founder-ceo.jpg',
     },
     {
-        name : 'Wayne Barnett',
-        rule : 'Founder & CEO ',
-        image : 'wayne-barnett-founder-ceo.jpg'
+        name : 'Angela Caroll',
+        rule : 'Chief Editor ',
+        image : 'angela-caroll-chief-editor.jpg',
     },
     {
-        name : 'Wayne Barnett',
-        rule : 'Founder & CEO ',
-        image : 'wayne-barnett-founder-ceo.jpg'
+        name : 'Walter Gordon ',
+        rule : 'Office Manager ',
+        image : 'walter-gordon-office-manager.jpg',
     },
     {
-        name : 'Wayne Barnett',
-        rule : 'Founder & CEO ',
-        image : 'wayne-barnett-founder-ceo.jpg'
+        name : 'Angela Lopez',
+        rule : 'Social Media  ',
+        image : 'Manager  angela-lopez-social-media-manager.jpg',
     },
     {
-        name : 'Wayne Barnett',
-        rule : 'Founder & CEO ',
-        image : 'wayne-barnett-founder-ceo.jpg'
+        name : 'Scott Estrada ',
+        rule : 'Developer  ',
+        image : 'scott-estrada-developer.jpg',
+    },
+    {
+        name : 'Barbara Ramos',
+        rule : 'Graphic Designer',
+        image : 'barbara-ramos-graphic-designer.jpg',
     },
 ];
 
-console.log(team);
+
+for (let index = 0; index < team.length; index++) {
+    const user = team[index];
+
+    for(key in user){
+        console.log(key, ':', user[key])
+    }
+
+    console.log('-----');
+
+    
+}
